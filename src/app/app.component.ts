9900733c998308c2afea158b6adcb5c1440fd834
Bhuvanagiri <2166191@cognizant.com>
2022-11-28 10:51:56 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  @ViewChild('incomeForm') incomeForm!: NgForm;
   title = 'budgetCalculator';
   remainingAmount: number = 0;
-  incomeArray: number[] = [];
-  expensesArray: number[] = [];
+  incomeArray: any[] = [];
+  expensesArray: any[] = [];
+
+  add() {
+    if (this.incomeForm.value.expenseType === 'income') {
+      this.incomeArray.push({
+        amount: this.incomeForm.value.amount,
+        description: this.incomeForm.value.description,
+      });
+    }
+    if (this.incomeForm.value.expenseType === 'expense') {
+      this.expensesArray.push({
+        amount: this.incomeForm.value.amount,
+        description: this.incomeForm.value.description,
+      });
+    }
+
+    this.incomeForm.resetForm();
+
+    this.remainingAmount =
+      this.incomeArray
+        .map((income) => income.amount)
+        .reduce(
+          (previousValue, amount) => {
+            const sum = amount + previousValue;
+            return sum;
+          },
+
+          0
+        ) -
+      this.expensesArray
+        .map((expense) => expense.amount)
+        .reduce(
+          (previousValue, amount) => {
+            const sum = amount + previousValue;
+            return sum;
+          },
+
+          0
+        );
+  }
 }
